cbfb69bc4a8e884c07fd251a54540c604506fb58
ehsanullahsalih <salihehsanullah123@gmail.com>
2025-11-01 10:58:04 +0430

    Add files via upload

---
+++ JavaApplication14.java
@@ -0,0 +1,16 @@
+
+package javaapplication14;
+
+public class JavaApplication14 {
+
+    public static void main(String[] args) {
+        System.out.println("  JJJ\t  A \tV\t V\t   A");  
+        System.out.println("   J\t A A\t V\tV\t  A A");
+        System.out.println("   J    A   A \t  V    V\t A   A");
+        System.out.println(" J J   AAAAAAA     V  V     \tAAAAAAA");
+        System.out.println(" JJJ  A       A     VV         A       A");
+       
+    
+    }
+    
+}
