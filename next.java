cf7710f0f1bb5dde307b149fdf56eb4531a4ca28
ehsanullahsalih <salihehsanullah123@gmail.com>
2025-11-06 15:33:37 +0430

    Add files via upload

---
+++ next.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class next {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter str1 : ");
+        String str1 = input.next();
+        System.out.println(str1);
+        System.out.print("Enter Str2 : ");
+        String str2 = input.next();
+        System.out.println(str2);
+
+    }
+}
---
+++ next2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class next2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Enter Str: ");
+        String Str = input.next();
+
+        System.out.println(Str);
+        System.out.println( input.next());
+        System.out.println( input.next());
+    }
+
+
+}
