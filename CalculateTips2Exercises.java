76aec16a08c2ad05bd4a655a44294530f9a2da17
ehsanullahsalih <salihehsanullah123@gmail.com>
2025-11-15 22:39:24 +0430

    Add files via upload

---
+++ CalculateTips2Exercises.java
@@ -0,0 +1,26 @@
+
+
+        import java.util.Scanner;
+public class CalculateTips2Exercises {
+
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter gratuity rat");
+        double gratuityrat=input.nextDouble();
+        System.out.println("enter subtotal ");
+        double subtotal=input.nextDouble();
+
+        double gratuity=(gratuityrat*subtotal)/100;
+        double total=gratuity+subtotal;
+
+        System.out.println("gratuity "+gratuity);
+        System.out.println("total " +total);
+
+
+
+
+    }
+
+}
